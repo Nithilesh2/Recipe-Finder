e47de3eb2c01a301fa6a2ae86e8630fbc5474b57
nithilesh <sunnybnithilesh@gmail.com>
2024-05-16 18:52:55 +0530

    v3

--- src/components/Li.jsx
+++ src/components/Li.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 import style from "../css/Li.module.css";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
 const Li = (props) => {
+  const sourceRef = useRef();
+  const urlRef = useRef();
+  function sourceBtnPressed() {
+    let src = sourceRef.current.value;
+    const removedBlankSpace = src.replace(" ", "");
+    window.open(`https://${removedBlankSpace}.com`, "_blank");
+  }
+  function recipeBtnPressed() {
+    window.open(urlRef.current.value, "_blank");
+  }
   return (
     <>
       <li className={style.listItems}>
@@ -15,10 +25,22 @@ const Li = (props) => {
         />
         <h4 className={style.recipeName}>{props.label}</h4>
         <div className={style.buttons}>
-          <button type="button" className={style.button}>
+          <button
+            onClick={sourceBtnPressed}
+            type="button"
+            value={props.source}
+            className={style.button}
+            ref={sourceRef}
+          >
             Source
           </button>
-          <button type="button" className={style.button}>
+          <button
+            onClick={recipeBtnPressed}
+            type="button"
+            value={props.url}
+            className={style.button}
+            ref={urlRef}
+          >
             Recipe
           </button>
         </div>
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -4,8 +4,16 @@ import { useNavigate } from "react-router-dom";
 import Li from "./Li";
 import axios from "axios";
 import { HashLoader } from "react-spinners";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "../css/ToastStyles.css";
 
 const Recipe = () => {
+  const notifyTrue = () =>
+    toast.success("range is not kept so recipes will be upto 50", {
+      className: "Toastify__toast--custom",
+      progressClassName: "Toastify__progress-bar--custom",
+    });
   const override = {
     display: "block",
     margin: "0 auto",
@@ -17,21 +25,44 @@ const Recipe = () => {
   const navigate = useNavigate();
   const inputRef = useRef();
   const buttonRef = useRef();
+  const rangeRef = useRef();
 
   async function btnClicked() {
     const query = inputRef.current.value;
-    const url = `https://api.edamam.com/search?q=${query}&from=0&to=50&app_id=abc32a2c&app_key=d644645ec14fbf33a06f909660dd9521`;
-    setData(query);
-    setLoading(true);
-    try {
-      const res = await axios.get(url);
-      const recipes = res.data.hits.map((hit) => hit.recipe);
-      setRecipeData(recipes);
-    } catch (error) {
-      console.error("Error fetching recipes:", error);
-    } finally {
-      setLoading(false);
-      inputRef.current.value = "";
+    const dataRange = rangeRef.current.value;
+    if (query === "") {
+      alert("Please enter input");
+    } else {
+      if (dataRange === "") {
+        const url = `https://api.edamam.com/search?q=${query}&from=0&to=50&app_id=abc32a2c&app_key=d644645ec14fbf33a06f909660dd9521`;
+        notifyTrue();
+        setData(query);
+        setLoading(true);
+        try {
+          const res = await axios.get(url);
+          const recipes = res.data.hits.map((hit) => hit.recipe);
+          setRecipeData(recipes);
+        } catch (error) {
+          console.error("Error fetching recipes:", error);
+        } finally {
+          setLoading(false);
+          inputRef.current.value = "";
+        }
+      } else {
+        const url = `https://api.edamam.com/search?q=${query}&from=0&to=${dataRange}&app_id=abc32a2c&app_key=d644645ec14fbf33a06f909660dd9521`;
+        setData(query);
+        setLoading(true);
+        try {
+          const res = await axios.get(url);
+          const recipes = res.data.hits.map((hit) => hit.recipe);
+          setRecipeData(recipes);
+        } catch (error) {
+          console.error("Error fetching recipes:", error);
+        } finally {
+          setLoading(false);
+          inputRef.current.value = "";
+        }
+      }
     }
   }
 
@@ -84,6 +115,14 @@ const Recipe = () => {
               Search
             </button>
           </div>
+          <div className={style.range}>
+            <input
+              type="number"
+              placeholder="50"
+              className={style.recipesNeededNumber}
+              ref={rangeRef}
+            />
+          </div>
           <div className={style.searchResults}>
             <h2 className={style.recipeResults}>Recipe results for : {data}</h2>
           </div>
@@ -100,7 +139,13 @@ const Recipe = () => {
             ) : (
               <ul className={style.dataItems}>
                 {recipeData.map((data, index) => (
-                  <Li key={index} label={data.label} image={data.image} />
+                  <Li
+                    key={index}
+                    source={data.source}
+                    label={data.label}
+                    image={data.image}
+                    url={data.url}
+                  />
                 ))}
               </ul>
             )}
@@ -116,6 +161,18 @@ const Recipe = () => {
           &uarr;
         </button>
       </main>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </>
   );
 };
